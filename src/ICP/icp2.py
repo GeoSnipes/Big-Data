482af14c2946fc8f42a8f0f703f361649ea9ed35
GeoSnipes <geovanni.fortisyute@gmail.com>
2018-01-30 13:13:19 -0600

    icp2 code completion

--- src/ICP/icp2.py
+++ src/ICP/icp2.py
@@ -1,10 +1,15 @@
+from pyspark import SparkContext, SparkConf
+
 def printList(groups):
     print(groups[0], end="")
     for li in groups[1]:
         print(", "+li, end="")
 
-sc =SparkContext.getOrCreate()
-file = sc.textFile("phrases.txt")
+#sc =SparkContext.getOrCreate()
+conf = SparkConf().setAppName("ICP2").setMaster("local[*]")
+sc = SparkContext(conf=conf)
+
+file = sc.textFile("icp2_phrases.txt")
 wordGroup = file.flatMap(lambda line: line.split(" ")).map(lambda word: (word[0], word)).cache()
 wordGroup = wordGroup.groupByKey().mapValues(list)
 TWG = wordGroup.collect()
