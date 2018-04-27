c0176f0dc57c57c41dccd555ca1c83cc8e2cd0e6
GeoSnipes <geovanni.fortisyute@gmail.com>
2018-04-27 00:16:58 -0500

    change visuals of flask app

--- lab_assignments/Lab 6/Source/TensorflowServerandWeb/label_image.py
+++ lab_assignments/Lab 6/Source/TensorflowServerandWeb/label_image.py
@@ -73,9 +73,9 @@ def main():
 #  * Running on your ip address
 
 # or
-# if __name__ == '__main__':
+if __name__ == '__main__':
     #app.debug = True
-    # app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
     # app.run()
 
 # and use
